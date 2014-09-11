90227b09dfe4b5a2006b159b5f44e84597b6c348
Luce <Luce@172-26-40-153.dynapool.nyu.edu>
2014-09-11 15:51:29 -0400

    change your credentials here

--- credentials.h
+++ credentials.h
@@ -1,5 +1,5 @@
 /* Credentials for Xively Account */
 
-#define APIKEY [Put Your Key Here]
-#define FEEDID [Put Your FeedID Here]
-#define USERAGENT [Put Your USERAGENT Here]
+#define APIKEY "[Put Your API Key Here]"
+#define FEEDID "[Put Your FeedID Here]"
+#define USERAGENT "[Put Your USERAGENT Here]"
